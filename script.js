6f08ac21b180462bae1c5a97f014e00128c1af74
hoi-publication <70566880+hoi-publication@users.noreply.github.com>
2021-07-03 14:41:20 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ async function main() {
         name: document.getElementsByClassName('name'),
         form: document.getElementsByClassName('form'),
         description: document.getElementsByClassName('description'),
-        reldate: document.getElementsByClassName('reldate'),
+        //      reldate: document.getElementsByClassName('reldate'),
         code: document.getElementsByClassName('code')
     };
     const SOURCE = 'https://spreadsheets.google.com/feeds/list/1o0jmDO8pvUYujM6aLH7k86c5MQ6gs0gShXjDBPZ_pmU/1/public/full?alt=json';
-    const COLUMNS = ['name', 'form', 'description', 'reldate', 'code'];
+    const COLUMNS = ['name', 'form', 'description', 'code'];
     const DATA = await separateRowFromJson(SOURCE, COLUMNS);
     for (let i = 0; i < DATA.length; i++) {
 
@@ -18,7 +18,7 @@ async function main() {
         TARGET['name'][i].textContent = DATA[i]['name'];
         TARGET['form'][i].textContent = DATA[i]['form'];
         TARGET['description'][i].textContent = DATA[i]['description'];
-        TARGET['reldate'][i].textContent = DATA[i]['reldate'];
+        //      TARGET['reldate'][i].textContent = DATA[i]['reldate'];
         TARGET['code'][i].textContent = DATA[i]['code'];
 
 
@@ -43,9 +43,9 @@ async function separateRowFromJson(SOURCE, COLUMNS) {
         var newDiv3 = document.createElement("div");
         newDiv3.className = 'description';
         document.getElementById('content').append(newDiv3);
-        var newDiv4 = document.createElement("div");
-        newDiv4.className = 'reldate';
-        document.getElementById('content').append(newDiv4);
+        //     var newDiv4 = document.createElement("div");
+        //     newDiv4.className = 'reldate';
+        //     document.getElementById('content').append(newDiv4);
         var newDiv5 = document.createElement("div");
         newDiv5.className = 'code';
         document.getElementById('content').append(newDiv5);
