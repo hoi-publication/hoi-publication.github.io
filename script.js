3afa90a37a536df595da93483f82aa5ef2c027d6
hoi-publication <70566880+hoi-publication@users.noreply.github.com>
2021-07-05 15:06:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,28 +3,30 @@ window.addEventListener('DOMContentLoaded', main);
 
 async function main() {
     const TARGET = {
+        date: document.getElementsByClassName('date'),
         name: document.getElementsByClassName('name'),
         form: document.getElementsByClassName('form'),
         description: document.getElementsByClassName('description'),
         //      reldate: document.getElementsByClassName('reldate'),
-        code: document.getElementsByClassName('code')
+
     };
     const SOURCE = 'https://spreadsheets.google.com/feeds/list/1o0jmDO8pvUYujM6aLH7k86c5MQ6gs0gShXjDBPZ_pmU/1/public/full?alt=json';
-    const COLUMNS = ['name', 'form', 'description', 'code'];
+    const COLUMNS = ['date', 'name', 'form', 'description'];
     const DATA = await separateRowFromJson(SOURCE, COLUMNS);
     for (let i = 0; i < DATA.length; i++) {
 
-
+        TARGET['date'][i].textContent = DATA[i]['date'];
         TARGET['name'][i].textContent = DATA[i]['name'];
         TARGET['form'][i].textContent = DATA[i]['form'];
         TARGET['description'][i].textContent = DATA[i]['description'];
         //      TARGET['reldate'][i].textContent = DATA[i]['reldate'];
-        TARGET['code'][i].textContent = DATA[i]['code'];
+
 
 
     }
 }
 
+// 이하 부분에서 실제로 데이터를 출력하는 듯함
 async function separateRowFromJson(SOURCE, COLUMNS) {
     const FETCHED_SOURCE = await fetch(SOURCE);
     let temp = await FETCHED_SOURCE.json();
@@ -35,20 +37,21 @@ async function separateRowFromJson(SOURCE, COLUMNS) {
         _DATA[i] = {};
 
         var newDiv = document.createElement("div");
-        newDiv.className = 'form';
+        newDiv.className = 'date';
         document.getElementById('content').append(newDiv);
-        var newDiv2 = document.createElement("div");
-        newDiv2.className = 'name';
-        document.getElementById('content').append(newDiv2);
         var newDiv3 = document.createElement("div");
-        newDiv3.className = 'description';
+        newDiv3.className = 'name';
         document.getElementById('content').append(newDiv3);
+        var newDiv2 = document.createElement("div");
+        newDiv2.className = 'form';
+        document.getElementById('content').append(newDiv2);
+        var newDiv4 = document.createElement("div");
+        newDiv4.className = 'description';
+        document.getElementById('content').append(newDiv4);
         //     var newDiv4 = document.createElement("div");
         //     newDiv4.className = 'reldate';
         //     document.getElementById('content').append(newDiv4);
-        var newDiv5 = document.createElement("div");
-        newDiv5.className = 'code';
-        document.getElementById('content').append(newDiv5);
+
 
         for (var k = 0; k < Object.keys(COLUMNS).length; k++) {;
             _DATA[i][COLUMNS[k]] = temp[i]['gsx$' + COLUMNS[k]]['$t'];
