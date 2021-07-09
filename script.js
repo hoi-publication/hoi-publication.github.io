79a3effe1ab495f7e5a4921f615edfbf2613b64a
hoi-publication <70566880+hoi-publication@users.noreply.github.com>
2021-07-09 20:39:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -32,9 +32,11 @@ async function separateRowFromJson(SOURCE, COLUMNS) {
     temp = temp['feed']['entry'];
 
     let _DATA = [];
+    // 이하 2개의 반복문은 JSON에서 행과 열을 각각 불러내는 부분인 것 같음
     for (var i = 0; i < Object.keys(temp).length; i++) {
         _DATA[i] = {};
 
+        //행을 기준으로 DIV를 연속으로 생성
         var newDiv = document.createElement("div");
         newDiv.className = 'date';
         document.getElementById('content').append(newDiv);
@@ -58,6 +60,7 @@ async function separateRowFromJson(SOURCE, COLUMNS) {
         for (var k = 0; k < Object.keys(COLUMNS).length; k++) {;
 
             _DATA[i][COLUMNS[k]] = temp[i]['gsx$' + COLUMNS[k]]['$t'];
+            // 반복문 안에서 (i,1) div 호출, (i,4)div '내용'으로 a href를 거는 방법...을...생각해보자...
 
         }
     }
